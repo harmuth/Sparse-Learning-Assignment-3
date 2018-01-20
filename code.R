8437d4d7893153a208e6f924dd0e969b917ac2e5
harmuth <philip@harmuth.io>
2018-01-20 22:37:28 +0100

    Lavet sparse PCA kode

--- code.R
+++ code.R
@@ -51,3 +51,58 @@ ggplot(class.pc, aes(pc1,pc2,col=same)) +
 
 
 
+ORS.PCA <- function(X, t, max.iter, convergence.threshold) {
+  over.threshold <- TRUE
+  v <- matrix(1/dim(X)[2], ncol = 1, nrow = dim(X)[2])
+  m <- 1
+  u <- X %*% v / sqrt(sum((X %*% v)^2))
+  v.f <- function (lambda, y) {
+    sign(t(X) %*% y) * pmax(t(X) %*% y - lambda, 0)
+  }
+  while(over.threshold | m > max.iter) {
+    new.u <- X %*% v / sqrt(sum((X %*% v) ^ 2))
+    if(max(v.f(0, new.u)) < t){
+      lambda <- 0
+    } else {
+      opt.func <- function(lambda){ abs(max(v.f(lambda, new.u)) - t)}
+      lambda <- optimise(opt.func, c(0, max(v.f(0, new.u))))$minimum
+    }
+    
+    new.v <- v.f(lambda, new.u)
+    
+    if((max(abs(new.v-v))<convergence.threshold) & (max(abs(new.u-u)) < convergence.threshold)){
+      over.threshold = FALSE
+    }
+    cat("iter =",m,"\n","v convergence:",max(abs(new.v-v)),"\n","u convergence:",max(abs(new.u-u)),"\n", "lambda:", lambda,"\n")
+    v <- new.v
+    u <- new.u
+    m <- m + 1
+  }
+  return(list(v = v, u = u, convergence = !over.threshold, n_iter = m - 1))
+}
+
+res <- ORS.PCA(as.matrix(X.residuals), 10, 100, 0.0001)
+
+MRS.PCA <- function(X, k, t, max.iter, convergence.threshold) {
+  
+  # initial projection
+  proj <- diag(nrow(X))
+  
+  # ready for output
+  u <- matrix(NA, ncol = k, nrow = nrow(X))
+  v <- matrix(NA, ncol = k, nrow = ncol(X))
+  for(i in 1:k){
+    new.X <- proj %*% X
+    tmp.res <- ORS.PCA(new.X, t, max.iter, convergence.threshold)
+    u[,i] <- tmp.res$u
+    v[,i] <- tmp.res$v
+    
+    # update projection
+    proj <- proj-u[,i] %*% t(u[,i])
+  }
+  
+  return(list(v = v, u = u))
+}
+
+MRS.PCA(as.matrix(X.residuals), 2, 1000, 100, 0.0001)
+
